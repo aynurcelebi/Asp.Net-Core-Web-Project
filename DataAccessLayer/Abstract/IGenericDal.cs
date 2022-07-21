08eab5b90915bfc7b35c36958365ea21d3379bf9
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-07-21 23:43:34 +0300

    Generic tanımlandı.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    // T bir class'a ait bütün değerleri üzerine alabilir.
+    // Aslında burada bir sınıf gönreceğim bu sınıfta Entity'den gelen bir sınıf olacak
+    public interface IGenericDal<T> where T : class 
+    {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+}
