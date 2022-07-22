6fb68a67608b5965843ee0797c5b1ca7f1708557
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-07-22 22:51:47 +0300

    Generic repository bitti.

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -19,22 +19,29 @@ namespace DataAccessLayer.Repository
 
         public T GetByID(int id)
         {
-            throw new NotImplementedException();
+            using var c = new Context();
+            return c.Set<T>().Find(id);
         }
 
         public List<T> GetList()
         {
-            throw new NotImplementedException();
+            using var c = new Context();
+            return c.Set<T>().ToList();
         }
 
         public void Insert(T t)
         {
-            throw new NotImplementedException();
+
+            using var c = new Context();
+            c.Add(t);
+            c.SaveChanges();
         }
 
         public void Update(T t)
         {
-            throw new NotImplementedException();
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges ();
         }
     }
 }
