c77ec9b0e8ea893bd272d0b0cbb9a4b2a9bdcbc3
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-07-22 22:43:59 +0300

    Generic Repository oluşturuldu.

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace DataAccessLayer.Abstract
 {
     // T bir class'a ait bütün değerleri üzerine alabilir.
-    // Aslında burada bir sınıf gönreceğim bu sınıfta Entity'den gelen bir sınıf olacak
+    // Aslında burada bir sınıf göndreceğim bu sınıfta Entity'den gelen bir sınıf olacak
     public interface IGenericDal<T> where T : class 
     {
         void Insert(T t);
