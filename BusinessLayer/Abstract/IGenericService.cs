ebc679cb7dd07725e065d6aa5c27ad8e1fdfc27e
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-07-23 23:31:08 +0300

    Generic service oluşturuldu.

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Bussiness tarafında CRUD operasyonları için gerekli olan metotların çağırılmadan önce imzasını taşıyacak
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T GetByID(int id);
+    }
+}
