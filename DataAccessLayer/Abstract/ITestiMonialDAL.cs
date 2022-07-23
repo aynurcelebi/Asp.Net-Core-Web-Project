112f2976bcb85250a6f0ac23d08a1277f975949d
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-07-23 23:10:46 +0300

    Entity framework dal'ları eklendi.

--- DataAccessLayer/Abstract/ITestiMonialDAL.cs
+++ DataAccessLayer/Abstract/ITestiMonialDAL.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ITestiMonialDAL:IGenericDal<SocialMedia>
+    public interface ITestimonialDal : IGenericDal<Testimonial>
     {
     }
 }
