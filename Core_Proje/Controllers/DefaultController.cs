5180b21b76da23dfa8f8860520b22fd6b25021fd
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-07-27 23:30:42 +0300

    Add Themas

---
+++ Core_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    // Backend tarafında yazılacak kodlar burda olacak. Mimariyi tanımlamış olduğumuz metotları burda çağıracağız.
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
