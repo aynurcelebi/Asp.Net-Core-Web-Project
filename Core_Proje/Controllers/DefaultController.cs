4d07d43ea0ed47b5883bc0e119ba2e9bd5e19004
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-08-01 23:36:59 +0300

    Repalce index property

--- Core_Proje/Controllers/DefaultController.cs
+++ Core_Proje/Controllers/DefaultController.cs
@@ -5,9 +5,6 @@ namespace Core_Proje.Controllers
     // Backend tarafında yazılacak kodlar burda olacak. Mimariyi tanımlamış olduğumuz metotları burda çağıracağız.
     public class DefaultController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index => View();
     }
 }
